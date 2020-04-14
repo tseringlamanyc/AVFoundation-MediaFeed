8a0025750c63b12b05ef4594dab732f17bb12085
Tsering Lama <tseringlama@pursuit.org>
2020-04-14 12:27:41 -0400

    allocated core date swift files

---
+++ AVFoundation-MediaFeed/CDMediaObject+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  CDMediaObject+CoreDataClass.swift
+//  AVFoundation-MediaFeed
+//
+//  Created by Tsering Lama on 4/14/20.
+//  Copyright © 2020 Tsering Lama. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class CDMediaObject: NSManagedObject {
+
+}
