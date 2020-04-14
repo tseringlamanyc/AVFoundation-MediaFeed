fda7446012ba58b74aa32bffbf72bede055e8204
Tsering Lama <tseringlama@pursuit.org>
2020-04-13 23:21:48 -0400

    made media model

---
+++ AVFoundation-MediaFeed/MediaObject.swift
@@ -0,0 +1,17 @@
+//
+//  MediaObject.swift
+//  AVFoundation-MediaFeed
+//
+//  Created by Tsering Lama on 4/13/20.
+//  Copyright © 2020 Tsering Lama. All rights reserved.
+//
+
+import Foundation
+
+struct MediaObject {
+    let imageData: Data?
+    let videoURL: String?
+    let caption: String?
+    let id = UUID().uuidString
+    let createdDate = Date()
+}
